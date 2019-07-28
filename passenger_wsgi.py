3ee028e598b2205141ecacfda0ea42b9e01be246
Nikita <kylikov_nikita@mail.ru>
2019-07-28 20:08:42 +0300

    test 5

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys, os
-sys.path.append('/home/f/f950220q/open-gamer.ru/Flask') # указываем директорию с проектом
-sys.path.append('/home/f/f950220q/.local/lib/python3.6/site-packages') # указываем директорию с библиотеками, куда поставили Flask
-f = open('/home/f/f950220q/open-gamer.ru/tmp/restart.txt', 'w')
+sys.path.append('/Flask') # указываем директорию с проектом
+f = open('/tmp/restart.txt', 'w')
 f.close()
 from Flask import app as application # когда Flask стартует, он ищет application. Если не указать 'as application', сайт не заработает
 from werkzeug.debug import DebuggedApplication # Опционально: подключение модуля отладки

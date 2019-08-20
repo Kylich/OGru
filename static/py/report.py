fe42e16ef07acffc0f5db28a7a335b396e6f4e5d
Kylich <kylikov_nikita@mail.ru>
2019-08-20 08:49:01 +0300

    -boto

--- static/py/report.py
+++ static/py/report.py
@@ -2,14 +2,13 @@ def sending(EText, JT):
     from email.mime.multipart import MIMEMultipart
     from email.mime.text import MIMEText
     import smtplib
-    import os
-    from boto.s3.connection import S3Connection
+
 
     msg = MIMEMultipart()
     
     message = '%s' % '\n '.join(JT)
     
-    password = S3Connection(os.environ['S3_RollerPass'])
+    password = 'Gorod4Narodov'
     msg['From'] = "opengamerroller@gmail.com"
     msg['To'] = "opengamerreport@gmail.com"
     msg['Subject'] = EText
